a42964ff22f40636dc31ee3f90fde859a8be6307
voltemort <jovanbryanginting@gmail.com>
2024-10-04 09:52:53 +0700

    radius

--- Frontend/src/views/blog/BlogDetail.vue
+++ Frontend/src/views/blog/BlogDetail.vue
@@ -30,7 +30,7 @@ const blogData = blogdet.value[0];
       <div class="row mb-4 justify-content-center">
         <div class="col-12">
           <div class="card mb-4">
-            <img :src="blogData.post.image" class="card-img-top" alt="Blog Image" style="width: 100%; height: 75vh;" />
+            <img :src="blogData.post.image" class="card-img-top" alt="Blog Image" style="width: 100%; height: 75vh; border-radius: 20px;" />
             <div class="wrap-text">
               <div class="1-text" style="margin-top: 30px;">
                 <p class="card-text">{{ blogData.post.sectionContent }}</p>
