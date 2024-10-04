5335d0aeab0e1bc9b7686a3a92d4437ef353ffdd
Diptya Bagus Sumantry <diptyasoemantry@gmail.com>
2024-10-04 14:01:40 +0700

    back to development

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -3,46 +3,46 @@ import { createRouter, createWebHistory } from "vue-router";
 
 //define a routes
 const routes = [
-  {
-    path: "/",
-    name: "maintenance",
-    component: () => import("../views/Maintenance/Maintenance.vue"),
-  },
   // {
   //   path: "/",
-  //   name: "home",
-  //   component: () => import("../views/Home/Home.vue"),
+  //   name: "maintenance",
+  //   component: () => import("../views/Maintenance/Maintenance.vue"),
   // },
-  // {
-  //   path: "/dashboard",
-  //   name: "dashboard",
-  //   component: () => import("../views/Dashboard/Dashboard.vue"),
-  // },
-  // {
-  //   path: "/blog",
-  //   name: "blog",
-  //   component: () => import("../views/blog/Blog.vue"),
-  // },
-  // {
-  //   path: "/contact",
-  //   name: "contact",
-  //   component: () => import("../views/Contact/ContactView.vue"),
-  // },
-  // {
-  //   path: "/technology",
-  //   name: "technology",
-  //   component: () => import("../views/Technology/Technology.vue"),
-  // },
-  // {
-  //   path: "/product",
-  //   name: "prouct",
-  //   component: () => import("../views/Product/Product.vue"),
-  // },
-  // {
-  //   path: "/blogdetail",
-  //   name: "blogdetail",
-  //   component: () => import( "../views/blog/BlogDetail.vue"),
-  // },
+  {
+    path: "/",
+    name: "home",
+    component: () => import("../views/Home/Home.vue"),
+  },
+  {
+    path: "/dashboard",
+    name: "dashboard",
+    component: () => import("../views/Dashboard/Dashboard.vue"),
+  },
+  {
+    path: "/blog",
+    name: "blog",
+    component: () => import("../views/blog/Blog.vue"),
+  },
+  {
+    path: "/contact",
+    name: "contact",
+    component: () => import("../views/Contact/ContactView.vue"),
+  },
+  {
+    path: "/technology",
+    name: "technology",
+    component: () => import("../views/Technology/Technology.vue"),
+  },
+  {
+    path: "/product",
+    name: "prouct",
+    component: () => import("../views/Product/Product.vue"),
+  },
+  {
+    path: "/blogdetail",
+    name: "blogdetail",
+    component: () => import( "../views/blog/BlogDetail.vue"),
+  },
 ];
 
 //create router
