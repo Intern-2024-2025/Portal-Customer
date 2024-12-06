a613905b531977f5acfd1bf952ae116670d63dcc
voltemort <jovanbryanginting@gmail.com>
2024-12-06 10:19:28 +0700

    triger push

--- Dashboard/src/views/Product/ClientProduct.vue
+++ Dashboard/src/views/Product/ClientProduct.vue
@@ -113,7 +113,7 @@ onMounted(() => {
                       class="fill-current"
                       width="18"
                       height="18"
-                      viewBox="0 0 18 18" 
+                      viewBox="0 0 18 18"  
                       fill="none"
                       xmlns="http://www.w3.org/2000/svg"
                     >
