80f0318660a49170446e1071d83553038cad3145
Diptya Bagus Sumantry <diptyasoemantry@gmail.com>
2024-12-10 09:24:06 +0700

    sgkms update

--- Dashboard/src/views/Sgkms/SgkmsView.vue
+++ Dashboard/src/views/Sgkms/SgkmsView.vue
@@ -11,7 +11,7 @@ const selectedVersion = ref('V1.0');
 const dropdownVisible = ref(false);
 const versions = ref(['V1.0']);
 const selectedEndpoint = ref('');
-const endpoints = ref(['/agent/login/','/agent/refreshSession/', '/rng/', '/mac/generate/', '/seal/', '/unseal/','/tokenize/','/detokenize/','/sign','/verify/','/cert/sign/','/cert/verify/','/key/info/','/secret/get/','/external/key/generate','/external/mac/generate/','/external/tokenize','/external/detokenize/','/agent/sign/','/agent/verify-signature/']); 
+const endpoints = ref(['/agent/login/','/agent/refreshSession/', '/rng/', '/mac/generate/', '/seal/', '/unseal/','/tokenize/','/detokenize/','/sign','/verify/','/cert/sign/','/cert/verify/','/key/info/','/secret/get/','/external/key/generate','/external/mac/generate/','/external/tokenize','/external/detokenize/','/external/sign/','/external/verify/']); 
 
 // Variabel reaktif untuk response message dan data
 const responseData = ref<any>(null);  // Menyimpan respons JSON dari API
@@ -31,7 +31,7 @@ const endpointData = {
       { name: 'slotId', label: 'Slot Id', placeholder: 'the ID of the slot to be used for login' },
       { name: 'password', label: 'Password', placeholder: 'the password or PIN for the agent in this slot' },
     ],
-    data: { slotId: '', password: '' },
+    data: { slotId: null, password: '' },
     url: `/${selectedVersion.value}/agent/login`, 
   },
   '/agent/refreshSession/': {
@@ -40,14 +40,16 @@ const endpointData = {
       { name: 'sessionToken', label: 'Session Token', placeholder: ' The current session token of the agent' },
     ],
     data: { slotId: '', sessionToken: '' },
+    url: `/${selectedVersion.value}/agent/refreshSession`,
   },
   '/rng/': {
     fields: [
       { name: 'slotId', label: 'Slot Id', placeholder: 'The ID of the slot requesting the random number' },
       { name: 'sessionToken', label: 'Session Token', placeholder: 'The session token for authentication' },
-      { name: 'outputLength', label: 'Output Length', placeholder: ' The desired length of the generated random number' },
+      { name: 'length', label: 'Length', placeholder: ' The desired length of the generated random number' },
     ],
-    data: { slotId: '', sessionToken: '', outputLength: '' },
+    data: { slotId: '', sessionToken: '', length: '' },
+    url: `/${selectedVersion.value}/rng`,
   },
   '/mac/generate/': {
     fields: [
@@ -55,10 +57,78 @@ const endpointData = {
       { name: 'sessionToken', label: 'Session Token', placeholder: 'The session token for authentication' },
       { name: 'keyId', label: 'Key Id', placeholder: 'The ID of the key used for generating the MAC' },
       { name: 'hashAlgo', label: 'Hash Algo', placeholder: '(dropdown)The hashing algorithm used for the MAC' },
-      { name: 'macData', label: 'MAC Data', placeholder: 'The data for which the MAC will be generated' },
+      { name: 'data', label: 'MAC Data', placeholder: 'The data for which the MAC will be generated' },
     ],
     data: { slotId: '', sessionToken: '', keyId: '', hashAlgorithm: '', macData: '' },
+    url: `/${selectedVersion.value}/mac/generate`,
   },
+  // '/mac/verify/CMAC/HMAC-SHA256': {
+  //   fields: [
+  //     { name: 'slotId', label: 'Slot Id', placeholder: 'The ID of the slot associated with the request' },
+  //     { name: 'sessionToken', label: 'Session Token', placeholder: 'The session token for authentication' },
+  //     { name: 'keyId', label: 'Key Id', placeholder: 'The ID of the key used for generating the MAC' },
+  //     { name: 'hashAlgo', label: 'Hash Algo', placeholder: 'The hashing algorithm used for the MAC. Possible values: "CMAC", "HMAC-SHA256' },
+  //     { name: 'data', label: 'MAC Data', placeholder: 'The data for which the MAC will be generated' },
+  //     { name: 'mac', label: 'Base 64 MAC', placeholder: 'The Base64 URL-safe encoded MAC to verify' },
+  //   ],
+  //   data: { slotId: '', sessionToken: '', keyId: '', hashAlgorithm: '', macData: '' },
+  //   url: `/${selectedVersion.value}/mac/verify`,
+  // },
+  // '/mac/generate/GMAC-256': {
+  //   fields: [
+  //     { name: 'slotId', label: 'Slot Id', placeholder: 'The ID of the slot associated with the request' },
+  //     { name: 'sessionToken', label: 'Session Token', placeholder: 'The session token for authentication' },
+  //     { name: 'keyId', label: 'Key Id', placeholder: 'The ID of the key used for generating the MAC' },
+  //     { name: 'hashAlgo', label: 'Hash Algo', placeholder: '(dropdown)The hashing algorithm used for the MAC' },
+  //     { name: 'data', label: 'MAC Data', placeholder: 'The data for which the MAC will be generated' },
+  //     { name: 'mac', label: 'base 64 MAC', placeholder: 'The Base64 URL-safe encoded MAC to verify' },
+  //     { name: 'iv', label: 'Initialization Vector (iv)', placeholder: 'The initialization vector' },
+  //   ],
+  //   data: { slotId: '', sessionToken: '', keyId: '', hashAlgorithm: '', macData: '' },
+  //   url: `/${selectedVersion.value}/mac/verify`,
+  // },
+  // '/encrypt/symmetric-encryption': {
+  //   fields: [
+  //     { name: 'slotId', label: 'Slot Id', placeholder: 'The ID of the slot used for encryption' },
+  //     { name: 'sessionToken', label: 'Session Token', placeholder: 'The session token for authentication' },
+  //     { name: 'keyId', label: 'Key Id', placeholder: 'The ID of the key used for sealing the plaintext (AES-256-GCM or RSA algorithm)' },
+  //     // { name: 'plaintext', label: 'Plaintext', placeholder: 'An array of plaintext strings to encrypt' },
+  //   ],
+  //   data: { slotId: '', sessionToken: '', keyId: '', plaintext: [] },
+  //   url: `/${selectedVersion.value}/encrypt`,
+  // },
+  // '/encrypt/asymmetric-encryption': {
+  //   fields: [
+  //     { name: 'slotId', label: 'Slot Id', placeholder: 'The ID of the slot used for encryption' },
+  //     { name: 'sessionToken', label: 'Session Token', placeholder: 'The session token for authentication' },
+  //     { name: 'keyId', label: 'Key Id', placeholder: 'The ID of the key used for sealing the plaintext (AES-256-GCM or RSA algorithm)' },
+  //     // { name: 'plaintext', label: 'Plaintext', placeholder: 'An array of plaintext strings to encrypt' },
+  //   ],
+  //   data: { slotId: '', sessionToken: '', keyId: '', plaintext: [] },
+  //   url: `/${selectedVersion.value}/encrypt`,
+  // },
+  // '/encrypt/Asymmetric-encryption-with-Session-key': {
+  //   fields: [
+  //     { name: 'slotId', label: 'Slot Id', placeholder: 'The ID of the slot used for encryption' },
+  //     { name: 'sessionToken', label: 'Session Token', placeholder: 'The session token for authentication' },
+  //     { name: 'keyId', label: 'Key Id', placeholder: 'The ID of the key used for sealing the plaintext (AES-256-GCM or RSA algorithm)' },
+  //     { name: 'useSessionKey', label: 'Session Key', placeholder: ' Boolean flag, true if a session key should be used for encryption (applicable for RSA)' },
+  //     // { name: 'plaintext', label: 'Plaintext', placeholder: 'An array of plaintext strings to encrypt' },
+  //   ],
+  //   data: { slotId: '', sessionToken: '', keyId: '', plaintext: [] },
+  //   url: `/${selectedVersion.value}/encrypt`,
+  // },
+  // '/decrypt/symmetric-decryption': {
+  //   fields: [
+  //     { name: 'slotId', label: 'Slot Id', placeholder: 'The ID of the slot used for encryption' },
+  //     { name: 'sessionToken', label: 'Session Token', placeholder: 'The session token for authentication' },
+  //     { name: 'keyId', label: 'Key Id', placeholder: 'The ID of the key used for sealing the plaintext (AES-256-GCM or RSA algorithm)' },
+  //     { name: 'keyVersion', label: 'Key Version', placeholder: 'The version of the key used for decryption' },
+  //     { name: 'ciphertext', label: 'Ciphertext', placeholder: 'An array of plaintext strings to encrypt' },
+  //   ],
+  //   data: { slotId: '', sessionToken: '', keyId: '', plaintext: [] },
+  //   url: `/${selectedVersion.value}/seal`,
+  // },
   '/seal/': {
     fields: [
       { name: 'slotId', label: 'Slot Id', placeholder: 'The ID of the slot used for encryption' },
@@ -67,6 +137,7 @@ const endpointData = {
       { name: 'plaintext', label: 'Plaintext', placeholder: 'An array of plaintext strings to encrypt' },
     ],
     data: { slotId: '', sessionToken: '', keyId: '', plaintext: [] },
+    url: `/${selectedVersion.value}/seal`,
   },
   '/unseal/': {
     fields: [
@@ -75,6 +146,7 @@ const endpointData = {
       { name: 'ciphertext', label: 'Ciphertext', placeholder: '{{ciphertext_1}}, {{ciphertext_2}} // An array of ciphertext strings to decrypt' },
     ],
     data: { sessionToken: '', slotId: '', ciphertext: [] },
+    url: `/${selectedVersion.value}/unseal`,
   },
   '/tokenize/': {
     fields: [
@@ -85,6 +157,7 @@ const endpointData = {
       { name: 'formatChar', label: 'Format Char', placeholder: 'Format-preserving characters to maintain original text structure' },
     ],
     data: { sessionToken: '', slotId: '', keyId: '', plaintext: '', formatChar: '' },
+    url: `/${selectedVersion.value}/tokenize`,
   },
   '/detokenize/': {
     fields: [
@@ -94,6 +167,7 @@ const endpointData = {
       { name: 'ciphertext', label: 'Ciphertext', placeholder: '{{token}}, {{metadata}} // Metadata associated with the tokenization process' },
     ],
     data: { sessionToken: '', slotId: '', keyId: '', ciphertext: [] },
+    url: `/${selectedVersion.value}/detokenize`,
   },
   '/sign/': {
     fields: [
@@ -103,6 +177,7 @@ const endpointData = {
       { name: 'data', label: 'Data', placeholder: 'The data to be signed' },
     ],
     data: { sessionToken: '', slotId: '', keyId: '', data: '' },
+    url: `/${selectedVersion.value}/sign`,
   },
   '/verify/': {
     fields: [
@@ -113,6 +188,7 @@ const endpointData = {
       { name: 'signature', label: 'Signature', placeholder: 'The digital signature in Base64 format to verify' },
     ],
     data: { sessionToken: '', slotId: '', keyId: '', data: '', signature: '' },
+    url: `/${selectedVersion.value}/verify`,
   },
   '/cert/sign/': {
     fields: [
@@ -123,6 +199,7 @@ const endpointData = {
       { name: 'csr', label: 'CSR', placeholder: 'The Certificate Signing Request in PEM format' },
     ],
     data: { sessionToken: '', slotId: '', validityPeriod: '', keyId: '', csr: '' },
+    url: `/${selectedVersion.value}/cert/sign`,
   },
   '/cert/verify/': {
     fields: [
@@ -132,6 +209,7 @@ const endpointData = {
       { name: 'certificate', label: 'Certificate', placeholder: 'The digitally signed certificate in PEM format' },
     ],
     data: { sessionToken: '', slotId: '', keyId: '', certificate: '' },
+    url: `/${selectedVersion.value}/cert/verify`,
   },
   '/key/info/': {
     fields: [
@@ -141,6 +219,7 @@ const endpointData = {
       { name: 'keyVersion', label: 'Key Version', placeholder: '{{key_version}}' },
     ],
     data: { sessionToken: '', slotId: '', keyId: '', keyVersion: '' },
+    url: `/${selectedVersion.value}/key/info`,
   },
   '/secret/get/': {
     fields: [
@@ -149,6 +228,7 @@ const endpointData = {
       { name: 'secretId', label: 'Secret Id', placeholder: '{{secret_id}}' },
     ],
     data: { sessionToken: '', slotId: '', secretId: '' },
+    url: `/${selectedVersion.value}/secret/get`,
   },
   '/external/key/generate': {
     fields: [
@@ -159,6 +239,7 @@ const endpointData = {
       { name: 'wrappingKeyId', label: 'Wrapping Key Id', placeholder: '{{wrapping_key_id}}' },
     ],
     data: { sessionToken: '', slotId: '', algo: 'AES', algoLength: 256, wrappingKeyId: '' },
+    url: `/${selectedVersion.value}/external/key/generate`,
   },
   '/external/mac/generate/': {
     fields: [
@@ -170,6 +251,7 @@ const endpointData = {
       { name: 'data', label: 'Data', placeholder: '{{data_to_hash}}' },
     ],
     data: { sessionToken: '', slotId: '', wrappingKeyId: '', wrappedKey: '', hashAlgo: '', data: '' },
+    url: `/${selectedVersion.value}/external/mac/generate`,
   },
   '/external/tokenize': {
     fields: [
@@ -180,6 +262,7 @@ const endpointData = {
       { name: 'plaintext', label: 'Plaintext', placeholder: '[{ text: "{{plaintext_1}}", formatChar: "{{format_char}}" }, { text: "{{plaintext_2}}", formatChar: "{{format_char}}" }]' },
     ],
     data: { sessionToken: '', slotId: '', wrappingKeyId: '', wrappedKey: '', plaintext: [] },
+    url: `/${selectedVersion.value}/external/tokenize`,
   },
   '/external/detokenize/': {
     fields: [
@@ -190,8 +273,9 @@ const endpointData = {
       { name: 'ciphertext', label: 'Ciphertext', placeholder: '[{ token: "{{tokenized_data_1}}", metadata: "{{metadata_1}}" }, { token: "{{tokenized_data_2}}", metadata: "{{metadata_2}}" }]' },
     ],
     data: { sessionToken: '', slotId: '', wrappingKeyId: '', wrappedKey: '', ciphertext: [] },
+    url: `/${selectedVersion.value}/external/detokenize`,
   },
-  '/agent/sign/': {
+  '/external/sign/': {
     fields: [
       { name: 'sessionToken', label: 'Session Token', placeholder: '{{session_token}}' },
       { name: 'slotId', label: 'Slot Id', placeholder: '{{slot_id}}' },
@@ -200,8 +284,9 @@ const endpointData = {
       { name: 'data', label: 'Data', placeholder: '{{data_to_sign}}' },
     ],
     data: { sessionToken: '', slotId: '', wrappingKeyId: '', wrappedKey: '', data: '' },
+    url: `/${selectedVersion.value}/agent/sign`,
   },
-  '/agent/verify-signature/': {
+  '/external/verify/': {
     fields: [
       { name: 'sessionToken', label: 'Session Token', placeholder: '{{session_token}}' },
       { name: 'slotId', label: 'Slot Id', placeholder: '{{slot_id}}' },
@@ -210,6 +295,7 @@ const endpointData = {
       { name: 'data', label: 'Data', placeholder: '{{original_data}}' },
     ],
     data: { sessionToken: '', slotId: '', publicKeyOrCert: '', signature: '', data: '' },
+    url: `/${selectedVersion.value}/agent/verify-signature`,
   },
 };
 
@@ -223,11 +309,18 @@ watch(selectedEndpoint, (newEndpoint) => {
 const responseMessage = ref('');
 const sendRequest = async () => {
   const endpoint = endpointData[selectedEndpoint.value];
-
   if (!endpoint) {
     responseMessage.value = 'Invalid endpoint selected';
     return;
   }
+  
+  //string to integer
+  formData.slotId = parseInt(formData.slotId, 10)
+  formData.algoLength = parseInt(formData.algoLength, 10)
+  formData.length = parseInt(formData.length, 10)
+  formData.validityPeriod = parseInt(formData.validityPeriod, 10)
+  formData.keyVersion = parseInt(formData.keyVersion, 10)
+  return console.log(formData, endpoint.url)
 
   try {
     const response = await axiosInstance.post(`${endpoint.url}?token=${localStorage.getItem("token")}`, formData)
